b2e7f89613c0760533f3c9a92dc873684982678e
monotic1301 <42168581+monotic1301@users.noreply.github.com>
2024-09-05 22:05:12 +0900

    Title: 바이러스 검사, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240905/바이러스 검사/virus-detector.py
+++ 240905/바이러스 검사/virus-detector.py
@@ -4,6 +4,9 @@ boss, employee = map(int,input().split(' '))
 
 answer = 0
 for store in stores:
+    if store - boss < 0:
+        answer += 1
+        continue
     if (store-boss)%employee ==0:
         answer += (store-boss)//employee + 1
     else:
